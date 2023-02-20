16887ea144bfaba03194ee35f0bd89b71bab3526
Bruno <psbrunosouza@gmail.com>
2023-02-20 18:39:33 -0300

    feat: logs logs logs solved

---
+++ logs-logs-logs/logs_logs_logs.go
@@ -0,0 +1,31 @@
+package logs
+
+import "strings"
+
+// Application identifies the application emitting the given log.
+func Application(log string) string {
+
+	for _, value := range log {
+		if value == '❗' {
+			return "recommendation"
+		} else if value == '🔍' {
+			return "search"
+		} else if value == '☀' {
+			return "weather"
+		}
+	}
+
+	return "default"
+}
+
+// Replace replaces all occurrences of old with new, returning the modified log
+// to the caller.
+func Replace(log string, oldRune, newRune rune) string {
+	return strings.ReplaceAll(log, string(oldRune), string(newRune))
+}
+
+// WithinLimit determines whether or not the number of characters in log is
+// within the limit.
+func WithinLimit(log string, limit int) bool {
+	return len(strings.Split(log, "")) <= limit
+}
